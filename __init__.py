3eb22451f75559a2edd1183ca84b9f66652e051f
nicobeli <iblanco.nicolas@gmail.com>
2024-11-21 15:52:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -5,7 +5,10 @@ from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
                                                                                                                                        
-app = Flask(__name__)                                                                                                                  
+app = Flask(__name__) 
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
