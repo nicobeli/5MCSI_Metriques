defde071f00450f84ac777233db6c45b4b16b715
nicobeli <iblanco.nicolas@gmail.com>
2024-11-21 16:26:38 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ from flask import json
 from datetime import datetime
 from urllib.request import urlopen
 import sqlite3
-                                                                                                                                       
-app = Flask(__name__) 
-@app.route("/rapport/")
-def mongraphique():
-    return render_template("graphique.html")
+app = Flask(__name__)
+@app.route('/')
+def hello_world():
+    return render_template('hello.html') #comm2
+@app.route("/contact/")
+def MaPremiereAPI():
+    return render_template("forms.html")
 @app.route('/tawarano/')
 def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
@@ -19,14 +21,12 @@ def meteo():
         dt_value = list_element.get('dt')
         temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
         results.append({'Jour': dt_value, 'temp': temp_day_value})
-    return jsonify(results=results)                                                                                                                                       
-@app.route('/')
-def hello_world():
-    return render_template('hello.html') #comm2
-
-@app.route("/contact/")
-def MaPremiereAPI():
-    return "<h2>Ma page de contact</h2>"
-  
+    return jsonify(results=results)
+@app.route("/rapport/")
+def mongraphique():
+    return render_template("graphique.html")
+@app.route("/histogramme/")
+def histogramme():
+    return render_template("histogramme.html")
 if __name__ == "__main__":
   app.run(debug=True)
